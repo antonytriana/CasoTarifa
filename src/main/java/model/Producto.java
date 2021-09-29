523eb4c1347fff175ba72a7e95a09396420018f5
Antony Triana <antonytriana@gmail.com>
2021-09-29 16:40:33 -0500

    Se agrega Patron Builder a la Clase Producto

--- src/main/java/model/Producto.java
+++ src/main/java/model/Producto.java
@@ -1,37 +1,75 @@
 package model;
 
 /**
+ * Clase Producto para el Caso Tarifa.
  *
- * @author Antony
+ * @author Antony Triana <antonytriana@gmail.com>
+ * @version 1.0
+ * @since 2021
  */
 public class Producto {
+
     private String nombre;
     private double precio;
     private double IVA;
     private double descuento;
 
-    public Producto(String nombre, double precio, double IVA,
-            double descuento) {
-        setNombre(nombre);
-        setPrecio(precio);
-        setIVA(IVA);
-        setDescuento(descuento);
-    }
-    
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public Producto() {
     }
 
-    public void setPrecio(double precio) {
-        this.precio = precio;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setIVA(double IVA) {
-        this.IVA = IVA;
+    public double getPrecio() {
+        return precio;
     }
 
-    public void setDescuento(double descuento) {
-        this.descuento = descuento;
+    public double getIVA() {
+        return IVA;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public static class ProductoBuilder {
+
+        private String nombre;
+        private double precio;
+        private double IVA;
+        private double descuento;
+
+        public ProductoBuilder() {
+        }
+
+        public ProductoBuilder conNombre(String nombre) {
+            this.nombre = nombre;
+            return this;
+        }
+
+        public ProductoBuilder conPrecio(double precio) {
+            this.precio = precio;
+            return this;
+        }
+
+        public ProductoBuilder conIVA(double IVA) {
+            this.IVA = IVA;
+            return this;
+        }
+
+        public ProductoBuilder conDescuento(double descuento) {
+            this.descuento = descuento;
+            return this;
+        }
+
+        public Producto build() {
+            Producto producto = new Producto();
+            producto.nombre = this.nombre;
+            producto.precio = this.precio;
+            producto.IVA = this.IVA;
+            producto.descuento = this.descuento;
+            return producto;
+        }
     }
 }
